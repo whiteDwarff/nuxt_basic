6e4e677a091de5b1d393c6029c89f424a1107678
whiteDwarff <munstarrrrr@gmail.com>
2025-05-12 16:30:41 +0900

    NuxtLink 학습

--- nuxt/pages/about.vue
+++ nuxt/pages/about.vue
@@ -5,6 +5,20 @@
       <p class="q-mt-sm text-grey-8">
         Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
       </p>
+      <div class="column">
+        <!-- 
+          RouterLink : 내부 페이지로 이동, 외부 페이지로 이동 시 <a>태그를 사용해야함
+        -->
+        <RouterLink to="/">RouterLink Home</RouterLink>
+        <RouterLink to="https://youtube.com/@gymcoding"
+          >RouterLink Youtube</RouterLink
+        >
+        <br />
+        <NuxtLink to="/" prefetched-class="bg-yellow">NuxtLink Home</NuxtLink>
+        <NuxtLink to="https://youtube.com/@gymcoding"
+          >NuxtLink Youtube</NuxtLink
+        >
+      </div>
     </div>
   </q-page>
 </template>
---
+++ nuxt/pages/course/prefetching-3.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <div class="text-center q-py-xl">
+      <div class="text-h4">
+        {{ $route.path }}
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+console.log('prefetching-3');
+</script>
+
+<!-- component life cycle setup hook 내부에서 실행 -->
+<script setup lang="ts"></script>
+
+<style scoped></style>
